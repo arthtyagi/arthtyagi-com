eadbcb2c09e534551f59d32bf190474cab9f7de5
Arth Tyagi <arthtyagi7@gmail.com>
2023-06-15 05:56:08 +0530

    supported opentype

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -6,7 +6,7 @@ export const config = {
 };
 
 const font = fetch(
-  new URL('../../public/fonts/switzer-variable.woff2', import.meta.url)
+  new URL('../../public/fonts/switzer-bold.ttf', import.meta.url)
 ).then((res) => res.arrayBuffer());
 
 export default async function handler(req: NextRequest) {
